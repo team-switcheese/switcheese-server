6af8c3833a99d207d5506c56d5be6fe6783c0a11
linavell <linavell@naver.com>
2024-12-14 01:01:44 +0900

    feat(#3): 공통 응답 객체 제거 및 전역 예외 처리 구현
    - ApiResponse와 같은 공통 응답 객체 제거 (중복성 문제 해결)
    - 전역 예외 처리 시 ResponseEntity의 body에 ErrorResponse 반환
    - 세분화된 전역 예외 처리 핸들러 구현

--- src/main/java/com/switcheese/server/common/exception/BusinessExceptionHandler.java
+++ src/main/java/com/switcheese/server/common/exception/BusinessExceptionHandler.java
@@ -1,25 +1,80 @@
-package com.switcheese.server.common.exception;
+package com.couponify.couponapi.exception;
 
-import com.switcheese.server.common.response.ApiResponse;
+import com.switcheese.server.common.exception.BusinessException;
+import com.switcheese.server.common.exception.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
 @RestControllerAdvice
 public class BusinessExceptionHandler {
 
-  @ExceptionHandler(BusinessException.class)
-  public ApiResponse<Void> handleBusinessException(BusinessException ex) {
-    log.error(ex.getStatus().name(), ex.getMessage());
-    return ApiResponse.error(ex.getStatus().name(), ex.getMessage());
+  @ExceptionHandler({
+      HttpRequestMethodNotSupportedException.class,
+      HttpMediaTypeNotSupportedException.class,
+      HttpMediaTypeNotAcceptableException.class,
+      MissingPathVariableException.class,
+      MissingServletRequestParameterException.class,
+      MissingServletRequestPartException.class,
+      ServletRequestBindingException.class,
+      TypeMismatchException.class,
+      HttpMessageNotReadableException.class,
+      MethodArgumentNotValidException.class,
+      NoHandlerFoundException.class
+  })
+  public ResponseEntity<ErrorResponse> badRequestException(
+      Exception exception,
+      HttpServletRequest request
+  ) {
+    ErrorResponse errorResponse = ErrorResponse.fromBadRequestException(request, exception);
+
+    log.error("Bad Request", exception);
+
+    return ResponseEntity.status(HttpStatus.valueOf(errorResponse.statusCode()))
+        .body(errorResponse);
   }
 
-  @ExceptionHandler({Exception.class, RuntimeException.class})
-  public ApiResponse<Void> handleAllExceptions(Exception ex) {
-    ex.printStackTrace();
-    return ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.name(), ex.getMessage());
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponse> internalServerErrorException(
+      Exception exception,
+      HttpServletRequest request
+  ) {
+    ErrorResponse errorResponse = ErrorResponse.fromInternalServerErrorException(request,
+        exception);
+
+    log.error("Internal Server Error", exception);
+
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(errorResponse);
+  }
+
+  @ExceptionHandler(BusinessException.class)
+  public ResponseEntity<ErrorResponse> businessException(
+      BusinessException exception,
+      HttpServletRequest request
+  ) {
+    ErrorResponse errorResponse = ErrorResponse.fromBusinessException(request, exception);
+
+    log.error(errorResponse.toString());
+
+    return ResponseEntity.status(exception.getStatus())
+        .body(errorResponse);
   }
 
 }
